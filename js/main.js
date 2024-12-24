dc3ebfcc11cc7030f932963bbfd9ed76ca7acecb
Pasang-lama <lpasang182@gmail.com>
2024-12-24 11:26:16 +0545

    #add initial file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".banner-slider").owlCarousel({
+        items:1,
+        autoplay:true,
+        nav: true,
+        navText : ['<i class="fa-solid fa-arrow-up"></i>', '<i class="fa-solid fa-arrow-down"></i>']
+    });
+    
+  });
